4fb1d8173c396797d708db405e6f07d64c22145e
yi <yi>
2020-02-18 14:46:56 +0800

    learn go first

---
+++ src/go.mod
@@ -0,0 +1,5 @@
+module github.com/even-c/learnGo
+
+go 1.13
+
+require github.com/sirupsen/logrus v1.4.2 // indirect
---
+++ src/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/even-c/src/testlib"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	fmt.Println(testlib.Add(1, 2))
+	log.Info("IThome Iron man")
+}
